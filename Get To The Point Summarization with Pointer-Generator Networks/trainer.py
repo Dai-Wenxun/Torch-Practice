95c09ea319d183c9f8376a2f8fe81d8b75de4751
Dai-Wenxun <wxDai2001@gmail.com>
2021-11-07 17:05:57 +0800

    all done

--- Get To The Point Summarization with Pointer-Generator Networks/trainer.py
+++ Get To The Point Summarization with Pointer-Generator Networks/trainer.py
@@ -69,7 +69,6 @@ class Trainer:
             loss = self.model(data)
             total_loss += loss.item()
 
-            self._check_nan(loss)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_clip)
             self.optimizer.step()
@@ -85,7 +84,6 @@ class Trainer:
             for data in valid_data:
                 loss = self.model(data)
                 total_loss += loss.item()
-                self._check_nan(loss)
 
             valid_loss = total_loss / len(valid_data)
             ppl = np.exp(valid_loss)
@@ -112,10 +110,6 @@ class Trainer:
 
         if self.is_logger:
             self.logger.info('Checkpoint loaded. Resume training from epoch {}'.format(self.start_epoch))
-
-    def _check_nan(self, loss):
-        if torch.isnan(loss):
-            raise ValueError('Training loss is nan')
 
     def _save_generated_text(self, generated_corpus):
         with open(self.saved_text_file, 'w') as fin:
