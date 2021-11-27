911dfa0e60ad0f2ad6d7e319c993d4908bbc55ae
Dai-Wenxun <wxDai2001@gmail.com>
2021-11-27 23:06:28 +0800

    Update utils.py

--- new_project/utils.py
+++ new_project/utils.py
@@ -1,58 +1,39 @@
 import torch
 import random
-import datetime
 import numpy as np
 
-from torch.utils.data import DataLoader
 
-from data_utils import data_process
-
-
-def data_preparation(args, tokenizer):
-    train_data = None
-    if args.do_train:
-        train_dataset = data_process(args, tokenizer=tokenizer, set_type='train')
-        train_data = DataLoader(
-            dataset=train_dataset,
-            batch_size=args.per_gpu_train_batch_size,
-            shuffle=True,
-            drop_last=True
-        )
-
-    valid_data = None
-    if args.do_eval:
-        valid_dataset = data_process(args, tokenizer=tokenizer, set_type='valid')
-        valid_data = DataLoader(
-            dataset=valid_dataset,
-            batch_size=args.per_gpu_eval_batch_size,
-            shuffle=True,
-            drop_last=False
-        )
-
-    test_data = None
-    if args.do_test:
-        test_dataset = data_process(args, tokenizer=tokenizer, set_type='test')
-        test_data = DataLoader(
-            dataset=test_dataset,
-            batch_size=args.per_gpu_eval_batch_size,
-            shuffle=False,
-            drop_last=False
-        )
-
-    return train_data, valid_data, test_data
-
-
-def get_local_time():
-    cur = datetime.datetime.now()
-    cur = cur.strftime('%b-%d-%Y_%H-%M-%S')
-
-    return cur
-
-
-def init_seed(seed):
+def set_seed(seed: int):
+    """ Set RNG seeds for python's `random` module, numpy and torch"""
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
+
+def beautify(args):
+    args_info = '\n'
+    args_info += f"{'data_dir'}={args.data_dir}\n"
+    args_info += f"{'model_name_or_path'}={args.model_name_or_path}\n"
+    args_info += f"{'task_name'}={args.task_name}\n"
+    args_info += f"{'max_length'}={args.max_length}\n"
+    args_info += f"{'train_examples'}={args.train_examples}\n"
+    args_info += f"{'dev_examples'}={args.dev_examples}\n"
+    args_info += f"{'test_examples'}={args.test_examples}\n"
+    args_info += f"{'per_gpu_train_batch_size'}={args.per_gpu_train_batch_size}\n"
+    args_info += f"{'per_gpu_eval_batch_size'}={args.per_gpu_eval_batch_size}\n"
+    args_info += f"{'num_train_epochs'}={args.num_train_epochs}\n"
+    args_info += f"{'gradient_accumulation_steps'}={args.gradient_accumulation_steps}\n"
+    args_info += f"{'max_steps'}={args.max_steps}\n"
+    args_info += f"{'logging_steps'}={args.logging_steps}\n"
+    args_info += f"{'warmup_steps'}={args.warmup_steps}\n"
+    args_info += f"{'learning_rate'}={args.learning_rate}\n"
+    args_info += f"{'weight_decay'}={args.weight_decay}\n"
+    args_info += f"{'adam_epsilon'}={args.adam_epsilon}\n"
+    args_info += f"{'max_grad_norm'}={args.max_grad_norm}\n"
+    args_info += f"{'do_train'}={args.do_train}\n"
+    args_info += f"{'do_eval'}={args.do_eval}\n"
+    args_info += f"{'temperature'}={args.temperature}\n"
+
+    return args_info
