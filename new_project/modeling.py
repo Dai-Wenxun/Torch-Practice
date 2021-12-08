83d87021550b72c80bdbd4261e7b5dd687d1b6f0
Dai-Wenxun <wxDai2001@gmail.com>
2021-12-08 15:20:22 +0800

    Update modeling.py

--- new_project/modeling.py
+++ new_project/modeling.py
@@ -4,29 +4,52 @@ from logging import getLogger
 
 from trainer import Trainer
 from utils import set_seed
-from tasks import InputExample
-
+from tasks import InputExample, load_examples, DEV_SET
+from domain_adapt import API_Adapt
 
 logger = getLogger()
 
 
-def train_single_model(trainer: Trainer, train_data: List[InputExample], eval_data: List[InputExample] = None):
+def logger_helper(results, metrics, repetitions):
+    for res in results:
+        logger.info(res)
+    avg_scores = {metric: 0. for metric in metrics}
+    for rp in range(repetitions):
+        for metric in metrics:
+            avg_scores[metric] += results[rp]['scores'][metric] / float(repetitions)
+
+    logger.info([f"avg_{metric}': {round(avg_scores[metric], 3)}" for metric in metrics])
+
+
+def train_single_model(trainer: Trainer):
     args = trainer.args
-    results = []
+    fine_tune_results = []
+    fine_tune_with_adapted_results = []
 
-    for repetition in range(args.repetitions):
-        set_seed(args.seed[repetition])
-        logger.info(f'Repetition: {repetition} Seed: {args.seed[repetition]}')
+    eval_data, _ = load_examples(
+        args.task_name, args.data_dir, DEV_SET, num_examples=args.dev_examples)
 
-        results.append(trainer.train(train_data, eval_data=eval_data))
+    for domain_repetition in range(args.repetitions):
+        domain_seed = args.seed[domain_repetition] + 100
+        checkpoint_path = os.path.join(args.output_dir, f'Seed-{domain_seed}')
 
-    logger.info(results)
-    avg_scores = {metric: 0. for metric in args.metrics}
-    for rp in range(args.repetitions):
-        for metric in args.metrics:
-            avg_scores[metric] += results[rp]['scores'][metric] / float(args.repetitions)
+        logger.info('\nDomain adaptation start:')
+        train_data = API_Adapt(args.data_dir, checkpoint_path, args.model_name_or_path,
+                               args.task_name, args.max_length, 1.0-args.train_examples, domain_seed)
 
-    logger.info([f"avg_{metric}': {round(avg_scores[metric], 3)}" for metric in args.metrics])
+        for fine_tune_repetition in range(args.repetitions):
+            fine_tune_seed = args.seed[fine_tune_repetition]
+            set_seed(fine_tune_seed)
+            logger.info(f'Domain:{domain_repetition}, Seed:{domain_seed}'
+                        f' Finetune:{fine_tune_repetition}-Seed:{fine_tune_seed}')
 
+            logger.info("Fine tune without adaptation start: ")
+            fine_tune_results.append(trainer.train(train_data, eval_data=eval_data))
+            logger.info("Fine tune with adaptation start: ")
+            fine_tune_with_adapted_results.append(trainer.train(train_data, eval_data=eval_data,
+                                                                checkpoint_path=checkpoint_path))
 
-
+    logger.info('Fine tune without adaptation results:')
+    logger_helper(fine_tune_results, args.metrics, args.repetitions)
+    logger.info('Fine tune with adaptation results:')
+    logger_helper(fine_tune_with_adapted_results, args.metrics, args.repetitions)
