92a06eb8a52e8f88f2f76244d2202f382b116d69
Dai-Wenxun <wxDai2001@gmail.com>
2021-12-14 00:02:51 +0800

    Update preprocessor.py

--- new_project/preprocessor.py
+++ new_project/preprocessor.py
@@ -2,6 +2,7 @@ from typing import Union
 from abc import ABC, abstractmethod
 
 from tasks import InputExample, InputFeatures, OUTPUT_MODES
+from pvp import PVPS
 
 
 class Preprocessor(ABC):
@@ -9,6 +10,7 @@ class Preprocessor(ABC):
         self.tokenizer = tokenizer
         self.args = args
         self.label_map = {label: i for i, label in enumerate(args.label_list)}
+        self.pvp = PVPS[self.args.taskname](tokenizer, self.args.pattern_id)
 
     @abstractmethod
     def get_input_features(self, example: InputExample) -> InputFeatures:
@@ -47,50 +49,37 @@ class SequenceClassifierPreprocessor(Preprocessor):
         return InputFeatures(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids,
                              label=label, logits=logits)
 
-# class MLMPreprocessor(Preprocessor):
-#     """Preprocessor for models pretrained using a masked language modeling objective (e.g., BERT)."""
-#
-#     def get_input_features(self, example: InputExample) -> InputFeatures:
-#
-#         if priming:
-#             input_ids, token_type_ids = self.pvp.encode(example, priming=True)
-#             priming_data = example.meta['priming_data']  # type: List[InputExample]
-#
-#             priming_input_ids = []
-#             for priming_example in priming_data:
-#                 pe_input_ids, _ = self.pvp.encode(priming_example, priming=True, labeled=True)
-#                 priming_input_ids += pe_input_ids
-#
-#             input_ids = priming_input_ids + input_ids
-#             token_type_ids = self.wrapper.tokenizer.create_token_type_ids_from_sequences(input_ids)
-#             input_ids = self.wrapper.tokenizer.build_inputs_with_special_tokens(input_ids)
-#         else:
-#             input_ids, token_type_ids = self.pvp.encode(example)
-#
-#         attention_mask = [1] * len(input_ids)
-#         padding_length = self.wrapper.config.max_seq_length - len(input_ids)
-#
-#         if padding_length < 0:
-#             raise ValueError(f"Maximum sequence length is too small, got {len(input_ids)} input ids")
-#
-#         input_ids = input_ids + ([self.wrapper.tokenizer.pad_token_id] * padding_length)
-#         attention_mask = attention_mask + ([0] * padding_length)
-#         token_type_ids = token_type_ids + ([0] * padding_length)
-#
-#         assert len(input_ids) == self.wrapper.config.max_seq_length
-#         assert len(attention_mask) == self.wrapper.config.max_seq_length
-#         assert len(token_type_ids) == self.wrapper.config.max_seq_length
-#
-#         label = self.label_map[example.label] if example.label is not None else -100
-#         logits = example.logits if example.logits else [-1]
-#
-#         if labelled:
-#             mlm_labels = self.pvp.get_mask_positions(input_ids)
-#             if self.wrapper.config.model_type == 'gpt2':
-#                 # shift labels to the left by one
-#                 mlm_labels.append(mlm_labels.pop(0))
-#         else:
-#             mlm_labels = [-1] * self.wrapper.config.max_seq_length
-#
-#         return InputFeatures(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids,
-#                              label=label, mlm_labels=mlm_labels, logits=logits, idx=example.idx)
+
+class MLMPreprocessor(Preprocessor):
+    """Preprocessor for models pretrained using a masked language modeling objective (e.g., BERT)."""
+
+    def get_input_features(self, example: InputExample) -> InputFeatures:
+        input_ids, token_type_ids = self.pvp.encode(example)
+
+        attention_mask = [1] * len(input_ids)
+        padding_length = self.wrapper.config.max_seq_length - len(input_ids)
+
+        if padding_length < 0:
+            raise ValueError(f"Maximum sequence length is too small, got {len(input_ids)} input ids")
+
+        input_ids = input_ids + ([self.wrapper.tokenizer.pad_token_id] * padding_length)
+        attention_mask = attention_mask + ([0] * padding_length)
+        token_type_ids = token_type_ids + ([0] * padding_length)
+
+        assert len(input_ids) == self.wrapper.config.max_seq_length
+        assert len(attention_mask) == self.wrapper.config.max_seq_length
+        assert len(token_type_ids) == self.wrapper.config.max_seq_length
+
+        label = self.label_map[example.label] if example.label is not None else -100
+        logits = example.logits if example.logits else [-1]
+
+        if labelled:
+            mlm_labels = self.pvp.get_mask_positions(input_ids)
+            if self.wrapper.config.model_type == 'gpt2':
+                # shift labels to the left by one
+                mlm_labels.append(mlm_labels.pop(0))
+        else:
+            mlm_labels = [-1] * self.wrapper.config.max_seq_length
+
+        return InputFeatures(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids,
+                             label=label, mlm_labels=mlm_labels, logits=logits, idx=example.idx)
