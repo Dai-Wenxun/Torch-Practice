d9e8e1950e9d84d8af4ee5ea911fe3219958ea3a
Dai-Wenxun <wxDai2001@gmail.com>
2021-12-30 19:11:02 +0800

    Update run.sh

--- new_project/run.sh
+++ new_project/run.sh
@@ -1,12 +1,13 @@
 METHOD=$1
-ADAPT_METHOD=$2
-MODEL_NAME_OR_PATH=$3
-TASK=$4
-TRAIN_EXAMPLES=$5
+MODEL_NAME_OR_PATH=$2
+TASK=$3
+TRAIN_EXAMPLES=$4
+PATTERN_ID=$5
 
 DATA_ROOT='data/'
-TRAIN_BATCH_SIZE=2
+TRAIN_BATCH_SIZE=8
 EVAL_BATCH_SIZE=32
+UNLABELED_BATCH_SIZE=64
 ACCU=1
 MAX_STEPS=2500
 LOGGING_STEPS=50
@@ -14,15 +15,20 @@ LOGGING_STEPS=50
 if [ $TASK = "cola" ]; then
   DATA_DIR=${DATA_ROOT}cola
   SEQ_LENGTH=64
+  PATTERN_ID=0
 elif [ $TASK = "mnli" ] || [ $TASK = "mnli-mm" ]; then
   DATA_DIR=${DATA_ROOT}mnli
   SEQ_LENGTH=256
+  LOGGING_STEPS=100
+  PATTERN_ID=3
 elif [ $TASK = "mrpc" ]; then
   DATA_DIR=${DATA_ROOT}mrpc
   SEQ_LENGTH=128
+  PATTERN_ID=2
 elif [ $TASK = "sst-2" ]; then
   DATA_DIR=${DATA_ROOT}sst-2
   SEQ_LENGTH=64
+  PATTERN_ID=1
 elif [ $TASK = "sts-b" ]; then
   DATA_DIR=${DATA_ROOT}sts-b
   SEQ_LENGTH=64
@@ -30,29 +36,32 @@ elif [ $TASK = "qqp" ]; then
   DATA_DIR=${DATA_ROOT}qqp
   SEQ_LENGTH=256
   LOGGING_STEPS=100
+  PATTERN_ID=1
 elif [ $TASK = "qnli" ]; then
   DATA_DIR=${DATA_ROOT}qnli
   SEQ_LENGTH=128
+  PATTERN_ID=2
 elif [ $TASK = "rte" ]; then
   DATA_DIR=${DATA_ROOT}rte
   SEQ_LENGTH=128
+  PATTERN_ID=1
 elif [ $TASK = "wnli" ]; then
   DATA_DIR=${DATA_ROOT}wnli
   SEQ_LENGTH=64
 fi
 
 
-#CUDA_VISIBLE_DEVICES=$device
 python3 fire.py \
 --method $METHOD \
---adapt_method $ADAPT_METHOD \
 --data_dir $DATA_DIR \
 --model_name_or_path $MODEL_NAME_OR_PATH \
 --task_name $TASK \
 --max_length $SEQ_LENGTH \
 --per_gpu_train_batch_size $TRAIN_BATCH_SIZE \
 --per_gpu_eval_batch_size $EVAL_BATCH_SIZE \
+--per_gpu_unlabeled_batch_size $UNLABELED_BATCH_SIZE \
 --gradient_accumulation_steps $ACCU \
 --max_steps $MAX_STEPS \
 --train_examples $TRAIN_EXAMPLES \
+--pattern_id $PATTERN_ID \
 --logging_steps $LOGGING_STEPS
